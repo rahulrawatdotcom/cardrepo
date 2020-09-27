43ccefa2782033c16843b7a1b8f3b106938d882a
rahulrawatdotcom <69967529+rahulrawatdotcom@users.noreply.github.com>
2020-09-27 11:56:06 +0530

    Update app.directive.ts

--- pycli1/src/app/app.directive.ts
+++ pycli1/src/app/app.directive.ts
@@ -1,1 +1,40 @@
 
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+selector: '[moveNextByMaxLength]'
+})
+export class MoveNextByMaxLengthDirective {
+
+constructor(private _el: ElementRef) { }
+
+@HostListener('keyup', ['$event']) onKeyDown(e: any) {
+    if (e.srcElement.maxLength === e.srcElement.value.length) {
+
+        e.preventDefault();
+
+        let nextControl: any = e.srcElement.nextElementSibling;
+       // Searching for next similar control to set it focus
+        while (true)
+        {
+            if (nextControl)
+            {
+                if (nextControl.type === e.srcElement.type)
+                {
+                    nextControl.focus();
+                    return;
+                }
+                else
+                {
+                    nextControl = nextControl.nextElementSibling;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
+
+}
